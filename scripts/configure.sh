6cc2d7fe4a2df23078032de5a245fe515a5baa75
Abrar Bin Kabir <abrarbinkabir@proton.me>
2024-06-07 00:50:37 +0600

    Minor changes

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-if [ ! -d "$HOME/dots" ]; then
-    echo "Dotfiles does not exist." &&
-    echo "Navigating to $HOME" &&
-    cd $HOME &&
-    echo "Cloning Dotfiles repo into $HOME/dots" &&
-    git clone "https://github.com/abrarbinkabir/dots.git" $HOME/dots &&
-    echo "Copying fonts" &&
+main(){
+	echo "Copying fonts" &&
 	mkdir ~/.local/share/fonts/ &&
 	cp -Tr $HOME/dots/fonts/ $HOME/.local/share/fonts &&
 	echo "Creating symlinks" &&
 	rm -rf $HOME/.config/qtile &&
-	./$HOME/dots/symlink.sh
-else 
-	echo "Dotfiles exists."
-fi
-main(){
+	./$HOME/dots/symlink.sh &&
 	echo "Installing native packages" &&
 	sudo pacman -S $(cat $(find ~/dots/misc -type f -iname "npkglist*" -exec ls -t -1 {} + | head -n 1) | cut -d " " -f 1) &&
 	echo "installing yay" &&
