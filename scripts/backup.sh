ac4c12fa88a0301c75783b27f59a6112f5e52c25
Abrar Bin Kabir <abrarbinkabir@proton.me>
2024-06-11 11:41:24 +0600

    Minor changes

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,48 +2,76 @@
 
 # Specifies theme
 theme="$HOME/.config/rofi/applet-config.rasi"
-mesg="Backup" 
+message="Backup" 
 dt="$(date +%Y%m%d)"
 
 # Defines options
-option_1=" Pacman Package List"
-option_2=" Push Dotfiles Repo"
+options=(" Backup in nvme0n1p2" " Backup in team-c175" " Pacman Package List" " Push Dotfiles Repo")
+directories=("/mnt/nvme0n1p2" "/run/media/abrar/team-c175")
 
 # Rofi CMD
 rofi_cmd() {
-   rofi -theme-str 'listview {columns: 1; lines: 2;}' \
+   rofi -theme-str 'listview {columns: 1; lines: 4;}' \
 		-theme-str 'window {width: 400px;}' \
 		-dmenu \
-		-mesg "$mesg" \
+		-mesg "$message" \
 		-markup-rows \
 		-theme "$theme"
 }
 
-# Passes options to rofi dmenu
-run_rofi() {
-	echo -e "$option_1\n$option_2" | rofi_cmd
+backup() {
+    cd "$dir" || { echo "Directory $dir not found"; exit 1; }
+    restic -r . backup \
+        --files-from="$HOME/.config/restic/include.txt" \
+        --exclude-file="$HOME/.config/restic/exclude.txt"  &&
+    restic -r . unlock &&
+    restic -r . forget \
+        --keep-last 3 \
+        --prune &&
+    notify-send -u normal -a Restic -i bell "Snapshot created successfully"
 }
 
-# Actions
-chosen="$(run_rofi)"
-case ${chosen} in
-    $option_1)
-    	pacman -Qen > "$HOME/dots/misc/npkglist-${dt}.md" &&
-    	find "$HOME/dots/misc/" -type f -iname 'npkglist*' -exec ls -r {} + | tail -n +4 | xargs -I {} mv {} ~/.local/share/Trash/files/ &&
-    	pacman -Qem > "$HOME/dots/misc/fpkglist-${dt}.md" &&
-    	find "$HOME/dots/misc/" -type f -iname 'fpkglist*' -exec ls -r {} + | tail -n +4 | xargs -I {} mv {} ~/.local/share/Trash/files/
-    	if [ $? -eq 0 ] ; then
-			notify-send -u normal -a "Package List" -i bell -t 3000 "Backup successful!"
-		else
-			notify-send -u critical -a "Package List" -i bell -t 3000 "Backup unsuccessful!"
-		fi
+# Main function  
+main() {
+    # Run Rofi with options
+    selection=$(printf "%s\n" "${options[@]}" | rofi_cmd)
+    case $selection in
+         "${options[0]}")
+            dir="${directories[0]}"
+            backup
+            ;;
+        "${options[1]}")
+            dir="${directories[1]}"
+            if [ ! -d "$dir" ]; then
+                udisksctl mount -b /dev/sdb1 > /dev/null 2>&1 || {
+                    notify-send -u normal -a Restic -i bell "Please insert USB drive"
+                    exit 1
+                }
+            fi
+            backup
+            ;;
+        "${options[2]}")
+            pacman -Qen > "$HOME/dots/misc/npkglist-${dt}.md" &&
+            find "$HOME/dots/misc/" -type f -iname 'npkglist*' -exec ls -r {} + | tail -n +4 | xargs -I {} mv {} ~/.local/share/Trash/files/ &&
+            pacman -Qem > "$HOME/dots/misc/fpkglist-${dt}.md" &&
+            find "$HOME/dots/misc/" -type f -iname 'fpkglist*' -exec ls -r {} + | tail -n +4 | xargs -I {} mv {} ~/.local/share/Trash/files/
+            if [ $? -eq 0 ] ; then
+                notify-send -u normal -a "Package List" -i bell -t 3000 "Backup successful!"
+            else
+                notify-send -u critical -a "Package List" -i bell -t 3000 "Backup unsuccessful!"
+            fi
+            ;;
+	  "${options[3]}")
+            cd "$HOME/dots" && git add -A && git commit -m "Minor changes" && git push origin main
+        if [ $? -eq 0 ] ; then
+            notify-send -u normal -a "Dotfiles" -i bell -t 3000 "Pushed successfully!"
+        else
+            notify-send -u critical -a "Dotfiles" -i bell -t 3000 "Push unsuccessful!"
+        fi
         ;;
-	$option_2)
-		cd "$HOME/dots" && git add -A && git commit -m "Minor changes" && git push origin main
-	if [ $? -eq 0 ] ; then
-		notify-send -u normal -a "Dotfiles" -i bell -t 3000 "Pushed successfully!"
-	else
-		notify-send -u critical -a "Dotfiles" -i bell -t 3000 "Push unsuccessful!"
-	fi
-    ;;
-esac
+    esac
+}
+
+# Call the main function
+main
+
--- scripts/restic.sh
+++ scripts/restic.sh
@@ -1,17 +1,55 @@
 #!/bin/bash
 
-directory="/run/media/abrar/team-c175"
+# Specifies Theme
+theme="$HOME/.config/rofi/applet-config.rasi"
+message="Restic Backup"
 
-if [ ! -d "$directory" ]; then
-	udisksctl mount -b /dev/sdb1 > /dev/null 2>&1
-fi
+options=("Backup in nvme0n1p2" "Backup in team-c175")
+directories=("/mnt/nvme0n1p2" "/run/media/abrar/team-c175")
 
-cd $directory &&
-restic -r . backup \
-	--files-from="$HOME/.config/restic/include.txt" \
-	--exclude-file="$HOME/.config/restic/exclude.txt"  &&
-restic -r . unlock &&
-restic -r . forget \
-	--keep-last 3 \
-	--prune
+# Rofi command function
+rofi_cmd() {
+    rofi -theme-str 'listview {columns: 1; lines: 2;}' \
+        -dmenu \
+        -mesg "$message" \
+        -markup-rows \
+        -theme "$theme"
+}
+
+backup() {
+    cd "$dir" || { echo "Directory $dir not found"; exit 1; }
+    restic -r . backup \
+        --files-from="$HOME/.config/restic/include.txt" \
+        --exclude-file="$HOME/.config/restic/exclude.txt"  &&
+    restic -r . unlock &&
+    restic -r . forget \
+        --keep-last 3 \
+        --prune &&
+    notify-send -u normal -a Restic -i bell "Snapshot created successfully"
+}
+
+# Main function  
+main() {
+    # Run Rofi with options
+    selection=$(printf "%s\n" "${options[@]}" | rofi_cmd)
+    case $selection in
+        "${options[1]}")
+            dir="${directories[1]}"
+            if [ ! -d "$dir" ]; then
+                udisksctl mount -b /dev/sdb1 > /dev/null 2>&1 || {
+                    notify-send -u normal -a Restic -i bell "Please insert USB drive"
+                    exit 1
+                }
+            fi
+            backup
+            ;;
+        "${options[0]}")
+            dir="${directories[0]}"
+            backup
+            ;;
+    esac
+}
+
+# Call the main function
+main
 	
