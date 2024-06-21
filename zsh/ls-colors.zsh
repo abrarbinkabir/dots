8bd47d2837092ef3d80e3e457772a6ce98e7ffac
Abrar Bin Kabir <abrarbinkabir@proton.me>
2024-06-21 10:20:23 +0600

    Minor Changes

---
+++ zsh/ls-colors.zsh
@@ -0,0 +1,18 @@
+
+LS_COLORS='di=01;34:ex=00;31:*.tar=00;36:*.zip=00;36:*.gz=00;36:*.tar.gz=00;36:*.rar=00;36:*.iso=00;36:*.tar.xz=00;36:*.apk=00;36:*.txt=00;32:*.md=00;32:*.yml=00;32:*.htm=00;32:*.html=00;32:*.css=00;32:*.css=00;32:*.json=00;32:*.py=00;32:*.sql=00;32:*.pdf=00;32:*.epub=00;32:*.djvu=00;32:*.conf=00;32:*.rasi=00;32:*.odt=00;32:*.ods=00;32:*.odp=00;32:*.doc=00;32:*.docx=00;32:*.xlsx=00;32:*.csv=00;32:*.pptx=00;32:*.db=00;32:*.tex=00;32:*.vcf=00;32:*.m4a=00;36:*.mp3=00;36:*.wav=00;36:*.mp4=00;36:*.mkv=00;36:*.webm=00;36:*.avi=00;36:*.png=00;35:*.jpg=00;35:*.jpeg=00;35:*.webp=00;35:*.heic=00;35:*.gif=00;35:*.tif=00;35:*.svg=00;35:*.ai=00;35:*.eps=00;35:*.psd=00;35:*.ps=00;35:*.ttf=00;35:*.otf=00;35:';
+
+export LS_COLORS
+
+# Normal Colors
+# Foreground color codes
+# Black=30, Red=31, Green=32, Yellow=33, Blue=34, Magenta=35, Cyan=36, White=37, Default=39, Reset=0
+
+# Background color codes
+# Black=40, Red=41, Green=42, Yellow=43, Blue=44, Magenta=45, Cyan=46, White=47
+# Default=49, Reset=0
+
+# Bright Colors
+# Foreground color codes
+# Black=90, Red=91, Green=92, Yellow=93, Blue=94, Magenta=95, Cyan=96, White=97
+# Background color codes
+# Black=100, Red=101, Green=102, Yellow=103, Blue=104, Magenta=105, Cyan=106, White=107
