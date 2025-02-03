b507d86c592056deab81c8eb908d5a4e47ba9ae9
Abrar Bin Kabir <abrarbinkabir@proton.me>
2025-02-03 10:07:14 +0600

    removed ignored files

--- scripts/tasklist.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-tasklist_file="$HOME/Documents/50-notes/60-journal/tasklist.md"
-if [[ -f "$tasklist_file" && -s "$tasklist_file" ]]; then
-    alacritty -e nvim -c 'norm Go- [ ]  ' -c 'startinsert' "$tasklist_file"
-
-elif [[ ! -s "$tasklist_file" ]]; then
-    alacritty -e nvim -c 'norm i- [ ]  ' -c 'startinsert' "$tasklist_file"
-fi
