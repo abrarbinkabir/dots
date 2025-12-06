2de14affb92dc5c49b36bb9802433a561d932651
Abrar Bin Kabir <abrarbinkabir@proton.me>
2025-12-06 10:35:42 +0600

    Minor changes

--- symlink.sh
+++ symlink.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the directories and files to link as a space-separated array
-directories=("alacritty" "bat" "betterlockscreen" "btop" "dunst" "fontconfig" "gtk-3.0" "kitty" "lf" "mpd" "mpv" "nsxiv" "nvim" "pandoc" "qtile" "qutebrowser" "redshift" "restic" "rofi" "scripts" "systemd" "yt-dlp" "zathura" "zoxide" "zsh")
+directories=("alacritty" "bat" "btop" "clipse" "dunst" "fontconfig" "galculator" "geany" gtk-3.0" "hypr" "kitty" "lf" "mpd" "mpv" "ncmpcpp" "nsxiv" "nvim" "obs-studio" "pandoc" "restic" "rofi" "scripts" "systemd" "waybar" "yt-dlp" "zathura" "zoxide" "zsh")
 
 # Loop through each config and create symbolic links
 for dir in "${directories[@]}"; do
